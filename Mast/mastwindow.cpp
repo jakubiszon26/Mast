40c0aef773683088c08271c6695349ebb1fe172f
Jakub Kołakowski <jakubkolakowski6@linux.pl>
2021-05-04 13:55:41 +0200

    ui changes

--- Mast/mastwindow.cpp
+++ Mast/mastwindow.cpp
@@ -1,11 +1,16 @@
 #include "mastwindow.h"
 #include "ui_mastwindow.h"
-
+#include <qpixmap.h>
 MastWindow::MastWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MastWindow)
 {
     ui->setupUi(this);
+    QPixmap picture(":/assets/images/mast_img.jpg");
+    int width = ui->mast_image->width();
+    int height = ui->mast_image->height();
+    picture = picture.scaled(width, height);
+    ui->mast_image->setPixmap(picture);
 }
 
 MastWindow::~MastWindow()
