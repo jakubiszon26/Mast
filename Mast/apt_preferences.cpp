9eab8803bc306171424278ba8fbfcf379791f8ea
Jakub Kołakowski <jakubkolakowski6@linux.pl>
2021-05-05 12:33:33 +0200

    turn on or turn off additional repository

--- Mast/apt_preferences.cpp
+++ Mast/apt_preferences.cpp
@@ -1,6 +1,8 @@
 #include "apt_preferences.h"
 #include "ui_apt_preferences.h"
 #include <QDirIterator>
+#include <QListWidgetItem>
+#include<QDebug>
 
 Apt_Preferences::Apt_Preferences(QWidget *parent) :
     QWidget(parent),
@@ -8,6 +10,7 @@ Apt_Preferences::Apt_Preferences(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->listWidget->clear();
+    //show aditional repos in list
     QDirIterator iterator("/etc/apt/sources.list.d/");
     while(iterator.hasNext()){
     ui->listWidget->addItem(iterator.next());
@@ -17,4 +20,48 @@ Apt_Preferences::Apt_Preferences(QWidget *parent) :
 Apt_Preferences::~Apt_Preferences()
 {
     delete ui;
+}
+
+void Apt_Preferences::on_listWidget_itemClicked(QListWidgetItem *item)
+{
+    //save selected item text in variable
+    selectedFile = item->text();
+}
+
+void Apt_Preferences::on_on_and_off_button_clicked()
+{
+  bool isOn = true;
+  QString line, repo;
+  QFile file(selectedFile);
+
+  file.open(QIODevice::ReadOnly | QIODevice::Text);
+  QTextStream in(&file);
+  do{
+      line = in.readLine();
+      if(line.startsWith("deb")){
+           repo = line;
+           qDebug() << "found "+line.toUtf8();
+           isOn = true;
+      }else if (line.startsWith("#deb") || line.startsWith("# deb")){
+         isOn = false;
+         repo = line;
+         qDebug() << "found" +line.toUtf8();
+      }
+  }while (!line.isNull());
+
+    file.close();
+    file.open(QFile::WriteOnly);
+    if (isOn == true){
+        QString output = "#" + repo;
+        qDebug() << "writing " + output;
+        file.write(output.toUtf8());
+    }else{
+        QString output = repo.remove(0, 1);
+        qDebug() << "writing " + output;
+        file.write(output.toUtf8());
+    }
+    file.close();
+
+
+
 }
--- Mast/apt_preferences.h
+++ Mast/apt_preferences.h
@@ -2,6 +2,10 @@
 #define APT_PREFERENCES_H
 
 #include <QWidget>
+#include <QListWidgetItem>
+
+
+static QString selectedFile = NULL;
 
 namespace Ui {
 class Apt_Preferences;
@@ -14,6 +18,10 @@ class Apt_Preferences : public QWidget
 public:
     explicit Apt_Preferences(QWidget *parent = nullptr);
     ~Apt_Preferences();
+private slots:
+    void on_listWidget_itemClicked(QListWidgetItem *item);
+
+    void on_on_and_off_button_clicked();
 
 private:
     Ui::Apt_Preferences *ui;
